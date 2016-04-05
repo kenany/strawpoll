0f98cc02cffcec6b6349510f1d877729acc0ae32
Kenan Yildirim <kenan@kenany.me>
2016-04-05 13:33:29 -0400

    set Content-Type to json
    Fixes https://github.com/KenanY/strawpoll/issues/15
    Closes https://github.com/KenanY/strawpoll/pull/16

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 var hyperquest = require('hyperquest');
-var querystring = require('querystring');
 
 var URL = 'http://strawpoll.me/api/v2/polls';
 var HEADERS = {
-  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  'Content-Type': 'application/json;charset=UTF-8',
   'X-Requested-With': 'node-strawpoll'
 };
 
 function create(options) {
   var stream = hyperquest.post(URL, {headers: HEADERS});
-  stream.end(querystring.stringify(options));
+  stream.end(JSON.stringify(options));
   return stream;
 }
 
 function get(id) {
   var stream = hyperquest.get(URL + '/' + id);
   return stream;
-};
+}
 
 create.create = create;
 create.get = get;
